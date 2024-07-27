7851089d330482592a18647822cd07a8a09d4de2
Faris Fadhail <farisfadhail@gmail.com>
2024-07-27 19:01:25 +0700

    slicing done

--- src/components/component/footer.jsx
+++ src/components/component/footer.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Footer() {
 	return (
 		<footer id="footer" className="gap-2 sm:flex-row py-6 w-full shrink-0 px-4 md:px-6 border-t space-y-4">
-			<nav className="sm:ml-auto flex gap-4 sm:gap-6 justify-center">
+			<nav className="sm:ml-auto md:flex gap-4 sm:gap-6 justify-center hidden">
 				<div className="flex space-x-4 items-center">
 					<Link href="#problem" className="text-xs hover:underline underline-offset-4" prefetch={false}>
 						Problem
